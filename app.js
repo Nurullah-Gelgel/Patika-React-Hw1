83605cf6bc9859ea82e162ebbf793e256f69b69e
Nurullah-Gelgel <74796585+Nurullah-Gelgel@users.noreply.github.com>
2022-07-07 23:56:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import axios from "axios"
+
+export async function getData(userId){
+    const {data:user } = await axios (
+        `https://jsonplaceholder.typicode.com/users/${userId}`);
+
+    const {data:post } = await axios (
+        `   https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+
+   
+        return {user,post};
+
+}
+
+
